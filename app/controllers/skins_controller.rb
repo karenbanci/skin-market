55fd4f155171ccaf96aefed34e27f893b09729ac
Bernardo Lima <bernardomonclaro@gmail.com>
2021-11-22 18:29:29 -0300

    Fiz o funcionar o new e o create do skin

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -1,9 +1,20 @@
 class SkinsController < ApplicationController
   def new
-  @skin = Skin.new
+    @skin = Skin.new
   end
 
   def create
-  
+    @skin = Skin.new(skin_params)
+    if @skin.save
+      redirect_to skin_path(@skin)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def skin_params
+    params.require(:skin).permit(:name, :price, :comment)
   end
 end
