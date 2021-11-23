ef11c3b68f27d2a45be0d318c320c22fb7938aba
Eduardo Cavalcante <ducu_yusuke@hotmail.com>
2021-11-23 18:08:23 -0300

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'skins#index'
-<<<<<<< HEAD
-  resources :skins
-=======
-
-  resources :skins, shallow: true do 
+  resources :skins, shallow: true do
     resources :orders
   end
->>>>>>> db4d541966f13345ad0eeb384cb3bf27360a55ec
 end
