a7b5d16c21a272f54812f951a500cc4f6a4b5d00
Bernardo Lima <bernardomonclaro@gmail.com>
2021-11-24 16:53:37 -0300

    perfil-usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'skins#index'
+  get "profile", to: "pages#profile", as: :profile
   resources :skins, shallow: true do
     resources :orders
   end
