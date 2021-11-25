297beb1cf8d152277428fc96fdd8b62fa8863dc5
Bernardo Lima <bernardomonclaro@gmail.com>
2021-11-25 15:39:46 -0300

    paginaperfil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # name device
+  before_action :configure_permitted_parameters, if:  :devise_controller?
   skip_before_action :authenticate_user!, only: %i[index]
   include Pundit
 
@@ -16,6 +18,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+# name device
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
